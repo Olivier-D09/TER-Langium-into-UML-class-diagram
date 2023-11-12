08c8d0b32fddac80f9bdaa858f2490a4e874e1fa
olivier <olivier.dalmau@etu.univ-cotedazur.fr>
2023-11-12 11:50:13 +0100

    microsoft lsp module add

--- TerFirst/out/extension.js
+++ TerFirst/out/extension.js
@@ -1,27 +1,122 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+exports.activate = void 0;
 const vscode = require("vscode");
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const lsp = require("vscode-languageserver");
+const tokenTypes = new Map();
+const tokenModifiers = new Map();
+const legend = (function () {
+    const tokenTypesLegend = [
+        'comment', 'string', 'keyword', 'number', 'regexp', 'operator', 'namespace',
+        'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
+        'method', 'decorator', 'macro', 'variable', 'parameter', 'property', 'label'
+    ];
+    tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index));
+    const tokenModifiersLegend = [
+        'declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated',
+        'modification', 'async'
+    ];
+    tokenModifiersLegend.forEach((tokenModifier, index) => tokenModifiers.set(tokenModifier, index));
+    return new vscode.SemanticTokensLegend(tokenTypesLegend, tokenModifiersLegend);
+})();
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "TerFirst" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('TerFirst.Try', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from TerFirstTry!');
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'semanticLanguage' }, new DocumentSemanticTokensProvider(), legend));
 }
 exports.activate = activate;
-// This method is called when your extension is deactivated
-function deactivate() { }
-exports.deactivate = deactivate;
+class DocumentSemanticTokensProvider {
+    async provideDocumentSemanticTokens(document, token) {
+        const allTokens = this._parseText(document.getText());
+        const builder = new vscode.SemanticTokensBuilder();
+        allTokens.forEach((token) => {
+            builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
+        });
+        return builder.build();
+    }
+    /**
+   * .
+   *
+   * @param x - The first input number
+   * @param y - The second input number
+   * @returns The arithmetic mean of `x` and `y`
+   *
+   */
+    _encodeTokenType(tokenType) {
+        if (tokenTypes.has(tokenType)) {
+            return tokenTypes.get(tokenType);
+        }
+        else if (tokenType === 'notInLegend') {
+            return tokenTypes.size + 2;
+        }
+        return 0;
+    }
+    _encodeTokenModifiers(strTokenModifiers) {
+        let result = 0;
+        for (let i = 0; i < strTokenModifiers.length; i++) {
+            const tokenModifier = strTokenModifiers[i];
+            if (tokenModifiers.has(tokenModifier)) {
+                result = result | (1 << tokenModifiers.get(tokenModifier));
+            }
+            else if (tokenModifier === 'notInLegend') {
+                result = result | (1 << tokenModifiers.size + 2);
+            }
+        }
+        return result;
+    }
+    /**
+   * . translate text onto IParsedToken frame
+   *
+   * @param text - text of a documment
+   *
+   * @returns the list of tokens informations on the IParsedToken frame
+   *
+   */
+    _parseText(text) {
+        const r = []; // initialisation de la structure des tokens
+        const lines = text.split(/\r\n|\r|\n/);
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+            let currentOffset = 0;
+            do {
+                const openOffset = line.indexOf('[', currentOffset); // openoffset se positionne sur le [ d'un token
+                if (openOffset === -1) {
+                    break;
+                }
+                const closeOffset = line.indexOf(']', openOffset); // closeoffset se positionne sur le ] d'un token
+                if (closeOffset === -1) {
+                    break;
+                }
+                const tokenData = this._parseTextToken(line.substring(openOffset + 1, closeOffset)); // 
+                //				console.log("t",tokenData.tokenType) ;
+                r.push({
+                    line: i,
+                    startCharacter: openOffset + 1,
+                    length: closeOffset - openOffset - 1,
+                    tokenType: tokenData.tokenType,
+                    tokenModifiers: tokenData.tokenModifiers
+                });
+                currentOffset = closeOffset; // se positionne derrère le token traité  
+            } while (true);
+        }
+        const tmp = lsp;
+        //lsp.SemanticTokensRequest ;
+        //extensions.getExtension("langium.langium-vscode");
+        console.log(tmp);
+        return r;
+    }
+    /**
+   * .extract pos of a token in a text
+   *
+   * @param text - string of a token
+   *
+   * @returns a token with tokenType and tokenModifiers area
+   *
+   */
+    _parseTextToken(text) {
+        const parts = text.split('.');
+        return {
+            tokenType: parts[0],
+            tokenModifiers: parts.slice(1)
+        };
+    }
+}
 //# sourceMappingURL=extension.js.map
--- TerFirst/src/extension.ts
+++ TerFirst/src/extension.ts
@@ -1,33 +1,141 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { lookupService } from 'dns';
+import { Server } from 'http';
 import * as vscode from 'vscode';
+import * as lsp from 'vscode-languageserver';
+import * as doc from 'vscode-languageserver-textdocument';
+import * as types from 'vscode-languageserver-types';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
+const tokenTypes = new Map<string, number>();
+const tokenModifiers = new Map<string, number>();
+
+const legend = (function() {
+	const tokenTypesLegend = [
+		'comment', 'string', 'keyword', 'number', 'regexp', 'operator', 'namespace',
+		'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
+		'method', 'decorator', 'macro', 'variable', 'parameter', 'property', 'label'
+	];
+	tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index)); 
+
+	const tokenModifiersLegend = [
+		'declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated',
+		'modification', 'async'
+	];
+	tokenModifiersLegend.forEach((tokenModifier, index) => tokenModifiers.set(tokenModifier, index));
+
+	return new vscode.SemanticTokensLegend(tokenTypesLegend, tokenModifiersLegend);
+})();
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "TerFirst" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('TerFirst.Try', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TerFirstTry!');
-	});
-
-	let try1 = vscode.commands.registerCommand('TerFirst.essai',() => {
-		vscode.window.createOutputChannel('try',);
-	});
-
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(try1);
-
-	
+	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'semanticLanguage' }, new DocumentSemanticTokensProvider(), legend));
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+interface IParsedToken {
+	line: number;
+	startCharacter: number;
+	length: number;
+	tokenType: string;
+	tokenModifiers: string[];
+}
+
+class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
+	async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
+		const allTokens = this._parseText(document.getText());
+		const builder = new vscode.SemanticTokensBuilder();
+		allTokens.forEach((token) => {
+			builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
+		});
+		return builder.build();
+	}
+	/**
+   * .
+   *
+   * @param x - The first input number
+   * @param y - The second input number
+   * @returns The arithmetic mean of `x` and `y`
+   *
+   */
+	private _encodeTokenType(tokenType: string): number {
+		if (tokenTypes.has(tokenType)) {
+			return tokenTypes.get(tokenType)!;
+		} else if (tokenType === 'notInLegend') {
+			return tokenTypes.size + 2;
+		}
+		return 0;
+	}
+
+	private _encodeTokenModifiers(strTokenModifiers: string[]): number {
+		let result = 0;
+		for (let i = 0; i < strTokenModifiers.length; i++) {
+			const tokenModifier = strTokenModifiers[i];
+			if (tokenModifiers.has(tokenModifier)) {
+				result = result | (1 << tokenModifiers.get(tokenModifier)!);
+			} else if (tokenModifier === 'notInLegend') {
+				result = result | (1 << tokenModifiers.size + 2);
+			}
+		}
+		return result;
+	}
+
+	/**
+   * . translate text onto IParsedToken frame 
+   *
+   * @param text - text of a documment
+   * 
+   * @returns the list of tokens informations on the IParsedToken frame
+   *
+   */
+	private _parseText(text: string): IParsedToken[] {
+		const r: IParsedToken[] = [];					// initialisation de la structure des tokens
+		const lines = text.split(/\r\n|\r|\n/);
+		for (let i = 0; i < lines.length; i++) {
+			const line = lines[i];
+			let currentOffset = 0;
+			do {
+				const openOffset = line.indexOf('[', currentOffset); // openoffset se positionne sur le [ d'un token
+				if (openOffset === -1) {
+					break;
+				}
+				const closeOffset = line.indexOf(']', openOffset); // closeoffset se positionne sur le ] d'un token
+				if (closeOffset === -1) {
+					break;
+				}
+				const tokenData = this._parseTextToken(line.substring(openOffset + 1, closeOffset)); // 
+//				console.log("t",tokenData.tokenType) ;
+				r.push({
+					line: i,
+					startCharacter: openOffset + 1,
+					length: closeOffset - openOffset - 1,
+					tokenType: tokenData.tokenType,
+					tokenModifiers: tokenData.tokenModifiers
+				});
+				currentOffset = closeOffset; // se positionne derrère le token traité  
+			} while (true);
+		}
+
+		
+		
+		const tmp = lsp ;
+		//lsp.SemanticTokensRequest ;
+		//extensions.getExtension("langium.langium-vscode");
+		console.log(tmp);
+		return r;
+	}
+	/**
+   * .extract pos of a token in a text  
+   *
+   * @param text - string of a token
+   * 
+   * @returns a token with tokenType and tokenModifiers area
+   *
+   */
+
+	private _parseTextToken(text: string): { tokenType: string; tokenModifiers: string[]; } {
+		const parts = text.split('.'); 
+		return {
+			tokenType: parts[0],
+			tokenModifiers: parts.slice(1)
+		};
+	}
+
+}
